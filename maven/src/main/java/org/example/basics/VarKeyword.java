451cf51dbd0cc50638a9e6933724ed63c5f56be3
AlexMikhaylov <alx.mikhaylov@gmail.com>
2024-08-13 00:33:38 +0200

    Add VarKeyword.java

---
+++ maven/src/main/java/org/example/basics/VarKeyword.java
@@ -0,0 +1,13 @@
+package org.example.basics;
+
+public class VarKeyword {
+    public static void main(String[] args) {
+        // var was introduced in Java 10
+        // local variable type inference
+        // Alt + Enter in IntelliJ var can be replaced with corresponding data type
+        var i = 12;
+        var s = "hello";
+        var d = 'a';
+        var c = 123.233;
+    }
+}

26ced557cb15d40db067e940f61cd7f36a4d0f68
AlexMikhaylov <alx.mikhaylov@gmail.com>
2024-08-13 00:21:11 +0200

    Add method refactoring short keys in IntelliJ

--- maven/src/main/java/org/example/basics/MethodRefactoring.java
+++ maven/src/main/java/org/example/basics/MethodRefactoring.java
@@ -2,6 +2,9 @@ package org.example.basics;
 
 public class MethodRefactoring {
     public static void main(String[] args) {
-
+        // Rename with intelliJ Shift + F6
+        // Change method signature dialog Ctrl + F6
+        // Extract code blocks to Method: Alt + Enter -> Exract -> Nethod or Ctrl + Alt + M
+        // Inlining methods Ctrl + Alt + N
     }
 }
